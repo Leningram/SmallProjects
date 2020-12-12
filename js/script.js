ea5dcd0ff1c51f103f42ffd28fcae0ff1fefa097
Kanat <yes.kanat@gmail.com>
2020-12-12 15:21:31 +0300

    jshint & script to get rest time

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@
 const deadline = '2020-12-31';
 
 function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date());
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor(t / (1000 * 60 * 60) % 24),
+        minutes = Math.floor(t / (1000 * 60) % 60),
+        seconds = Math.floor(t / 1000 % 60);
 
+    return {
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
 
 }
