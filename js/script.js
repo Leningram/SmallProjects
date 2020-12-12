40410e94a3905acfc2d1835b4613594903606915
Kanat <yes.kanat@gmail.com>
2020-12-12 15:51:21 +0300

    function getZero added

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const deadline = '2020-12-31';
 
 function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
+    const t = Date.parse(endtime) - Date.parse(new Date()) - (3 * 60 * 60 * 1000),
         days = Math.floor(t / (1000 * 60 * 60 * 24)),
         hours = Math.floor(t / (1000 * 60 * 60) % 24),
         minutes = Math.floor(t / (1000 * 60) % 60),
@@ -26,12 +26,20 @@ function setClock(selector, endtime) {
         timeInterval = setInterval(updateClock, 1000);
     updateClock();
 
+    function getZero(num) {
+        if (num < 10) {
+            return (`0${num}`);
+        } else {
+            return (num);
+        }
+    }
+
     function updateClock() {
         const t = getTimeRemaining(endtime);
-        days.innerHTML = t.days;
-        hours.innerHTML = t.hours;
-        minutes.innerHTML = t.minutes;
-        seconds.innerHTML = t.seconds;
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
 
         if (t <= 0) {
             clearInterval(timeInterval);
